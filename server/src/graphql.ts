1c5c40041e778b6da97bf1216b57dc6e9b1202da
Bobby <bobbysebolao@gmail.com>
2020-02-09 21:35:23 +0000

    Create graphql schema for Listings object type

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,11 +1,36 @@
-import { GraphQLObjectType, GraphQLString, GraphQLSchema } from "graphql";
+import {
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema
+} from "graphql";
+
+import { listings } from "./listings";
+
+const Listing = new GraphQLObjectType({
+  name: "Listing",
+  fields: {
+    id: { type: GraphQLNonNull(GraphQLID) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    image: { type: GraphQLNonNull(GraphQLString) },
+    address: { type: GraphQLNonNull(GraphQLString) },
+    price: { type: GraphQLNonNull(GraphQLInt) },
+    numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBeds: { type: GraphQLNonNull(GraphQLInt) },
+    numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+    rating: { type: GraphQLNonNull(GraphQLInt) }
+  }
+});
 
 const query = new GraphQLObjectType({
   name: "Query",
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => "Hello from the Query!"
+    listings: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+      resolve: () => listings
     }
   }
 });
@@ -13,9 +38,19 @@ const query = new GraphQLObjectType({
 const mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => "Hello from the Mutation!"
+    deleteListing: {
+      type: GraphQLNonNull(Listing),
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve: (_root, { id }) => {
+        for (let i = 0; i < listings.length; i++) {
+          if (listings[i] === id) {
+            return listings.splice(i, 1)[0];
+          }
+        }
+        throw new Error("failed to delete listing");
+      }
     }
   }
 });
