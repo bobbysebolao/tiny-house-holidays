f2373e827d0e408b9934c5c145ec7f17a53f537d
Bobby <bobbysebolao@gmail.com>
2020-02-09 21:55:50 +0000

    Refactor graphql schema using graphql schema language

---
+++ server/src/graphql/resolvers.ts
@@ -0,0 +1,20 @@
+import { IResolvers } from "apollo-server-express";
+import { listings } from "../listings";
+
+export const resolvers: IResolvers = {
+  Query: {
+    listings: () => {
+      return listings;
+    }
+  },
+  Mutation: {
+    deleteListing: (_root: undefined, { id }: { id: string }) => {
+      for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+          return listings.splice(i, 1)[0];
+        }
+      }
+      throw new Error("failed to delete listing");
+    }
+  }
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,34 +4,16 @@
 
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
-// import bodyParser from "body-parser";
-// import { listings } from "./listings";
+import { typeDefs, resolvers } from "./graphql";
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: "/api" });
-
-// app.use(bodyParser.json());
-
-// //listings
-// app.get("/listings", (_req, res) => {
-//   return res.send(listings);
-// });
-
-// //delete listing
-// app.post("/delete-listing", (req, res) => {
-//   const id: string = req.body.id;
-
-//   for (let i = 0; i < listings.length; i++) {
-//     if (listings[i].id === id) {
-//       return res.send(listings.splice(i, 1));
-//     }
-//     return res.send("Failed to delete listing");
-//   }
-// });
 
 app.listen(port);
 
 console.log(`App is running at port ${port}`);
+
+// typeDefs
+// resolvers
