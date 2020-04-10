01345677374953b8537bc3580210532c135d7108
Bobby <bobbysebolao@gmail.com>
2020-04-11 00:55:51 +0100

    Create Google Sign-In GraphQL fields

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,23 +1,13 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-  type Listing {
-    id: ID!
-    title: String!
-    image: String!
-    address: String!
-    price: Int!
-    numOfGuests: Int!
-    numOfBeds: Int!
-    numOfBaths: Int!
-    rating: Int!
-  }
 
   type Query {
-    listings: [Listing!]!
+    authUrl: String!
   }
 
   type Mutation {
-    deleteListing(id: ID!): Listing!
+    logIn: String!
+    logOut: String!
   }
 `;
