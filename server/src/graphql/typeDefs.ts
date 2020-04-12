e5f209f476efb78cccf771fb7576e2f8c08a54b7
Bobby <bobbysebolao@gmail.com>
2020-04-12 09:45:34 +0100

    Make gql resolver typedefs for querying user data (bookings and listings)

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -2,6 +2,55 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
 
+type Booking {
+  id: ID!
+  listing: Listing!
+  tenant: User!
+  checkIn: String!
+  checkOut: String!
+}
+
+type Bookings {
+  total: Int!
+  result: [Booking!]!
+}
+
+enum ListingType {
+  APARTMENT
+  HOUSE
+}
+
+type Listing {
+  id: ID!
+  title: String!
+  description: String!
+  image: String!
+  host: User!
+  type: ListingType!
+  address: String!
+  city: String!
+  bookings(limit: Int!, page: Int!): Bookings
+  bookingsIndex: String!
+  price: Int!
+  numOfGuests: Int!
+}
+
+type Listings {
+  total: Int!
+  result: [Listing!]!
+}
+
+type User {
+  id: ID!
+  name: String!
+  avatar: String!
+  contact: String!
+  hasWallet: Boolean!
+  income: Int
+  bookings(limit: Int!, page: Int!): Bookings
+  listings(limit: Int!, page: Int!): Listings!
+}
+
 type Viewer {
   id: ID
   token: String
@@ -16,7 +65,7 @@ input LogInInput {
 
   type Query {
     authUrl: String!
-    user: String!
+    user(id: ID!): User!
   }
 
   type Mutation {
