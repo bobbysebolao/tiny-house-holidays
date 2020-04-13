1fd0add00fb820675c83fa4cc2c4f1269e7913d9
Bobby <bobbysebolao@gmail.com>
2020-04-13 16:25:02 +0100

    Display highest priced listings on home page

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link, RouteComponentProps } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
 import { Col, Row, Layout, Typography } from "antd";
+import { LISTINGS } from "../../lib/graphql/queries";
+import { Listings as ListingsData, ListingsVariables } from "../../lib/graphql/queries/Listings/__generated__/Listings";
+import { ListingsFilter } from "../../lib/graphql/globalTypes";
 import { displayErrorMessage } from "../../lib/utils";
-import { HomeHero } from "./components";
+import { HomeHero, HomeListings, HomeListingsSkeleton } from "./components";
 
 import mapBackground from "./assets/map-background.jpg";
 import sanFranciscoImage from "./assets/san-francisco.jpg";
@@ -11,7 +15,18 @@ import cancunImage from "./assets/cancun.jpg";
 const { Content } = Layout;
 const { Paragraph, Title } = Typography;
 
+const PAGE_LIMIT = 4;
+const PAGE_NUMBER = 1;
+
 export const Home = ({ history }: RouteComponentProps) => {
+    const { loading, data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
+        variables: {
+            filter: ListingsFilter.PRICE_HIGH_TO_LOW,
+            limit: PAGE_LIMIT,
+            page: PAGE_NUMBER
+        }
+    });
+    
     const onSearch = (value: string) => {
         // .trim() is a native JS method that removes whitespaces from the start and end of a string
         const trimmedValue = value.trim();
@@ -22,6 +37,17 @@ export const Home = ({ history }: RouteComponentProps) => {
             displayErrorMessage("Please enter a valid search")
         }
     };
+
+    const renderListingsSection = () => {
+        if (loading) {
+            return <HomeListingsSkeleton />;
+        }
+
+        if (data) {
+            return <HomeListings title="Premium Listings" listings={data.listings.result} />
+        }
+        return null;
+    }
 
     return (
         <Content className="home" style={{ backgroundImage: `url(${mapBackground})`}}>
@@ -36,6 +62,8 @@ export const Home = ({ history }: RouteComponentProps) => {
                 </Paragraph>
                 <Link to="/listings/united%20states" className="ant-btn ant-btn-primary ant-btn-lg home__cta-section-button">Popular listings in the United States</Link>
             </div>
+
+            {renderListingsSection()}
 
             <div className="home__listings">
                 <Title level={4} className="home__listings-title">
