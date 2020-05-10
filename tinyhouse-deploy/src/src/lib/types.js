80b0c26d269501e0a4878231ac36c32137993af0
Bobby <bobbysebolao@gmail.com>
2020-05-10 15:31:22 +0100

    Push build for deployment

--- tinyhouse-deploy/src/src/lib/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// enum is a special Typescript type that lets you limit the values that a collection property can have to ones that you define.
-var ListingType;
-(function (ListingType) {
-    ListingType["Apartment"] = "APARTMENT";
-    ListingType["House"] = "HOUSE";
-})(ListingType = exports.ListingType || (exports.ListingType = {}));
