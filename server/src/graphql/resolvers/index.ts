9776c66da28328b6f3c98bf4ddbd354851f44159
Bobby <bobbysebolao@gmail.com>
2020-06-07 22:53:52 +0100

    Install missing lodash dev dependency

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,7 +1,7 @@
-import merge from "lodash.merge";
+import * as lodash from "lodash";
 import { bookingResolvers } from "./Booking";
 import { listingResolvers } from "./Listing";
 import { userResolvers } from "./User";
 import { viewerResolvers } from "./Viewer";
 
-export const resolvers = merge(bookingResolvers,listingResolvers, userResolvers, viewerResolvers);
+export const resolvers = lodash.merge(bookingResolvers,listingResolvers, userResolvers, viewerResolvers);
