df99f5434f97f8e91955f08206111aa699db2c53
Bobby <bobbysebolao@gmail.com>
2020-06-12 01:11:41 +0100

    Create Jest configs
    Adds a jest.config.js file that configures Jest to:
    
    - Read test files with TypeScript extensions (.ts, .tsx)
    - Run an additional setupTests.js config file to make custom
    jest matchers available from jest-dom inside tests

---
+++ client/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "preset": 'ts-jest',
+  "testEnvironment": 'jsdom',
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "setupFilesAfterEnv": ['<rootDir>/src/__tests__/config/setupTests.ts'],
+  "testPathIgnorePatterns": ["/src/__tests__/config/setupTests.ts"]
+}
